a153f7fca534f344242023c4033c37495d78a8e5
helga2lip <helga2lip@gmail.com>
2025-01-16 16:31:00 +0300

    task 1-8

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg'; /*декларативный*/
+import './App.css'; /*декларативный*/
+
+function App() { // императивный
+	const currentYear = new Date().getFullYear(); // декларативный
+  return (
+    <div className="App"> {/*декларативный*/}
+      <header className="App-header"> {/*декларативный*/}
+        <img src={logo} className="App-logo" alt="logo" /> {/*декларативный*/}
+        <p> {/*декларативный*/}
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a {/*декларативный*/}
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+		<div>{currentYear}</div> {/*декларативный*/}
+      </header>
+    </div>
+  );
+}
+
+export default App; /*декларативный*/
